6a2533e8502a513cf4c45fd2a6aa8f675ac41049
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-13 18:56:00 +0500

    object change

---
+++ DOM/objects.js
@@ -0,0 +1,28 @@
+ /* use object in array
+
+ const student=[
+   {
+    Name: "shehiryar",
+    rollno:123,
+    school: "RPS"
+    }
+ ]
+
+*/ 
+
+/*
+const player={
+    Name:"babar",
+    team:"pakistan",
+    no:56
+};
+console.log(player)
+console.log(player.team)
+*/
+
+
+const teams=new Object();
+teams.name= "pakistan",
+teams.wins= 72,
+console.log(teams.name)
+console.log(teams.wins)

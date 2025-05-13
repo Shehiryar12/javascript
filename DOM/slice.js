c5356679f53329f3e0445b74706f21eb1b3d9fcc
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-13 18:16:06 +0500

    slice code

---
+++ DOM/slice.js
@@ -0,0 +1,50 @@
+/*
+const fruits = ["apple", "banana", "cherry", "date", "fig", "grape"];
+
+const fruitsone=fruits.slice(1,4)
+console.log(fruitsone)
+
+//Use the .slice() method to extract a sub-array containing only "banana", "cherry", and "date".
+*/
+
+/*
+const sentence = "JavaScript is awesome!";
+const newsent=sentence.slice(13,21)
+console.log(newsent)
+*/
+// Use the .slice() method to extract the word "awesome" from the string and store it in a new variable.
+
+
+//---------------------alphabetic sort----------------------------------------------------------------------
+
+/*
+const player=["zahoor","hunain","raza","ejaz"]
+const playertwo=player.sort()
+console.log(playertwo)
+console.log(player)
+*/
+
+/*
+const golfplayer=["usman","ali","ahmed","rehman","bilal"]
+const golfplayertwo=golfplayer.reverse()
+console.log(golfplayertwo)
+console.log(golfplayer);
+*/
+
+/*
+const score=["12","4", "5", "6"];
+const scoretwo = score.toSorted();
+console.log(scoretwo)
+*/
+
+let numbers = [42, 15, 8, 23, 74, 16];
+const numbertwo=numbers.sort()
+console.log(numbertwo) 
+// Use the .sort() method to sort the numbers in ascending order and print the result.
+
+const names=["taha","hammad","inam","kashif"];
+const namestwo=names.sort()
+console.log(namestwo)
+
+
+

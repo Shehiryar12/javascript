b0d13aa37e0a81a909d40b2b5bbbb6e7927c0404
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-13 20:19:36 +0500

    data changed

--- DOM/objectmethod.js
+++ DOM/objectmethod.js
@@ -3,6 +3,10 @@ const student={
     score: 20,
     school: "RPS"
 }
+
+
+
+
 console.log(Object.entries(student))  //return an array of key value pairs.
 console.log(Object.keys(student))    //return  an arrays of object keys.
 console.log(Object.values(student))  //return an arrays of property value of the object. 

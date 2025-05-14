ce21d662b275575b5a63e9c03926f444f2cf8aca
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-14 17:44:34 +0500

    changes in fromentries

---
+++ DOM/objectfromentries.js
@@ -0,0 +1,8 @@
+const idcard=[
+    [32,21],
+    ['usman','ahmed'],
+    [18,25],
+]
+console.log(Object.fromEntries(idcard))
+
+// it is used to convert array into object, when we print them in object the numbers in array will print.

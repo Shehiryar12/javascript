ecd895358e01a542a954ff820f7047a4e39ed96d
Shehiryar12 <shehiryar.ranglerz@gmail.com>
2025-05-14 15:25:33 +0500

    objectassignchange

---
+++ DOM/objectassign.js
@@ -0,0 +1,13 @@
+ const data={
+    name:"ali",
+    lastname:"munawar",
+    age:23
+ };
+ const info={
+    name:"ahmed",
+    lastname:"zahid",
+    
+ };
+
+let create=Object.assign(data,info)
+console.log(create)
